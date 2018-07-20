3c4306fbdf866db844ba5fe5fea4ed6380f2622e
Lin Bao <lin.bao@yitu-inc.com>
2018-07-20 19:40:08 +0800

    init

---
+++ src/Demo/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './demo.scss';
+
+class Demo extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div className="demo">This is a simple demo.</div>
+    }
+}
+
+export default Demo;
